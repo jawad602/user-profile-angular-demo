25ae418b3c27836c96acbd93ee809d1065b9ad60
jawad602 <jawadjamil602@gmail.com>
2024-04-19 00:25:37 +0500

    minor update

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -56,6 +56,7 @@ export class ProfileComponent {
   deleteUser(id: any) {
     this.isSpinning = true;
     this.subs.sink = this.api_servicess.deleteUser(id).subscribe((response: any) => {
+      this.user_data = null;
       this.message.create('success', 'User Delete Succeesfully');
       this.loadSingleUser(this.user_id);
       this.isSpinning = false;
